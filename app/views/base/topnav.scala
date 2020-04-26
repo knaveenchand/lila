51b009b98c7f4789e29c295b8ee8e15924e66995
knaveenchand <knaveenchand@gmail.com>
2020-04-26 12:34:04 +0530

    Update topnav.scala

--- app/views/base/topnav.scala
+++ app/views/base/topnav.scala
@@ -20,7 +20,7 @@ object topnav {
         "/",
         frag(
           span(cls := "play")(trans.play()),
-          span(cls := "home")("lichess.org")
+          span(cls := "home")("chessATC.com")
         )
       ),
       div(role := "group")(
